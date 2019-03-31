6b0fa23916d08b186e7677ba16a8acfdc74ee775
Tara  ~ Assistant Research Professor <tara.furstenau@nau.edu>
2019-03-31 10:28:29 -0700

    remove ext path for bins

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='mtsv',
-      version='1.0',
+      version='1.0.2',
       description='Metagenomic analysis pipeline',
       author='Tara Furstenau',
       author_email='Tara.Furstenau@nau.edu',
@@ -26,16 +26,6 @@ setup(name='mtsv',
           'commands/snakefiles/*snek',
           'data/*']
       },
-      data_files=[
-          ('mtsv/ext', ['mtsv/ext/target/release/mtsv-binner',
-                   'mtsv/ext/target/release/mtsv-build',
-                   'mtsv/ext/target/release/mtsv-chunk',
-                   'mtsv/ext/target/release/mtsv-collapse',
-                   'mtsv/ext/target/release/mtsv-readprep',
-                   'mtsv/ext/target/release/mtsv-signature',
-                   'mtsv/ext/target/release/mtsv-tree-build',
-                   'mtsv/ext/mtsv-db-build'])
-      ],
       classifiers=[
           'Programming Language :: Python',
           'Development Status :: 4 - Beta',
